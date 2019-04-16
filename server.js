73cb35c2354da6856c8cc6c20878369b656adb5b
katherinestout <kjs14@hood.edu>
2019-04-16 19:34:27 -0400

    comments

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
+//dependencies
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const routes = require('./routes/handlers');
 
+//variables
+const routes = require('./routes/handlers');
 const app = express();
 
+//main layout
 const hbs = exphbs.create({
     defaultLayout:'main'
 });
 
-//app.use(express.static(''));
+//access to public folder
+app.use(express.static('public'));
 
+//access to views folder
 app.set('views', path.join(__dirname, 'views'));
 
+//set handlebars engine
 app.engine('handlebars', hbs.engine);
-
 app.set('view engine', 'handlebars');
 
 app.set('port', (process.env.PORT || 3000));
