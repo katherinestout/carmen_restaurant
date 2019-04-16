fcdab626f9b0be5071220c807681208a77804d39
katherinestout <kjs14@hood.edu>
2019-04-16 16:42:27 -0400

    first

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.get('/', function(req,res){
+    res.render('home');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Server started on port' + app.get('port'));
+});
